a32a0a7a900b9ce7d0a6466ee9e8ee15c9d70527
ngoctin02806 <ngoctin040999@gmail.com>
2020-08-27 19:08:48 +0700

    chore: fix some error

--- 01.UI/Group17.WebUI/routers/product.router.js
+++ 01.UI/Group17.WebUI/routers/product.router.js
@@ -6,5 +6,10 @@ const productController = require('../controllers/product.controller');
 
 router.get('/san-pham/:productId/:slug', productController.renderProductDetail);
 router.post('/san-pham/:productId/:slug', productController.postNewComment);
+router.post(
+  '/san-pham/:productId/:slug/cap-nhap',
+  productController.updateExistComment
+);
+router.post('/comments/:commentId', productController.deleteExistComment);
 
 module.exports = router;
